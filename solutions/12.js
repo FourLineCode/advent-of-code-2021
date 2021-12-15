b764fb341dab6c111620bace01e36b11e805b483
FourLineCode <akmal3535.ah@gmail.com>
2021-12-15 19:03:27 +0600

    backup

---
+++ solutions/12.js
@@ -0,0 +1,26 @@
+export function formatInput(rawInput) {
+	return rawInput.trim().split("\n");
+}
+
+export function solution1(input) {
+	return "TODO";
+}
+
+export function solution2(input) {
+	return "TODO";
+}
+
+export const input = `
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+`;
+
+export const output = {
+	part1: 0,
+	part2: 0,
+};
